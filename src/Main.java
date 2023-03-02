b89d9a5f4f548b9f96a3b317b1970fb309b8d020
LUM <lumlum177@gmail.com>
2023-03-02 22:00:45 +0300

    Коммит после выполнения

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
 
+        try{
+            throw new CustomException("It's exception");
+        } catch (CustomException e) {
+            System.out.println(e.getMessage());
+        }
+
     }
 }
